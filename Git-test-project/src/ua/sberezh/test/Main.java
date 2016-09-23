996e1164327b020b75cea1834943f1919de4e91e
Admin <Admin@microsof-87f72e>
2016-09-23 20:59:10 +0300

    added 1!

--- Git-test-project/src/ua/sberezh/test/Main.java
+++ Git-test-project/src/ua/sberezh/test/Main.java
@@ -3,6 +3,6 @@ package ua.sberezh.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!!");
 	}
 }
